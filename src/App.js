160bd7eab0605b2329850869d5912105b0ec0d5a
Suzanne Cabral <suzannecabral@live.com>
2020-10-12 20:04:42 -0700

    Save progress, starting next step

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
